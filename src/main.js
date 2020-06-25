9de0ef45f1ca8b61bf34046784c6acd1866e16e4
gabrielcilico <gabrielcilico@gmail.com>
2020-06-25 20:39:23 -0300

    Adicionando vuetify, axios e consumindo API do professor

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import axios from 'axios';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  axios,
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
